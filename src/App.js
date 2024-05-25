671c75e5c6c8374a7a3df941eb968142927039da
Nishant3105 <nishantkumar2522@gmail.com>
2024-05-25 13:35:49 +0530

    Loading

--- src/App.js
+++ src/App.js
@@ -4,47 +4,37 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies,updateMovies] = useState([])
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
+  const [movies,setMovies] = useState([])
+  const [loading,setLoading] = useState(false)
 
   const getMoviesHandler = async () => {
     // fetch('https://swapi.dev/api/films').then(res=>res.json()).then(ans=>console.log(ans))
-    let resp=await fetch('https://swapi.dev/api/films')
-    if(resp)
-    resp=await resp.json()
-    if(resp)
-    console.log(resp)
-    updateMovies(resp.results)
+    // let resp=await fetch('https://swapi.dev/api/films')
+    // if(resp)
+    // resp=await resp.json()
+    // if(resp)
+    // console.log(resp)
+    // setMovies(resp.results)
 
-    // try {
-    //   let resp = await fetch('https://swapi.dev/api/films');
-    //   if (resp.ok) {
-    //     let data = await resp.json();
-    //     console.log(data);
-    //     updateMovies(data.results);
-    //   }
-    // } catch (error) {
-    //   console.error('Error fetching films:', error);
-    // }
+    try {
+      setLoading(true)
+      let resp = await fetch('https://swapi.dev/api/films');
+      if (resp.ok) {
+        let data = await resp.json();
+        console.log(data);
+        setLoading(false)
+        setMovies(data.results);
+      }
+    } catch (error) {
+      console.error('Error fetching films:', error);
+    }
   }
 
 
   return (
     <React.Fragment>
       <section>
+        {loading && <h1>Loading... Please wait</h1>}
         <button onClick={getMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
